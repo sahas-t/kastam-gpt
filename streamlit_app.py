24fe600b54b43b923daaca04dc219df483b90408
Sahas <sahas@tamarind.ai>
2023-02-10 23:43:27 +0530

    Added input field

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 # create a streamlit app with a title and a max width of 1000px
-st.title('Streamlit App')
+st.title('Kastam GPT Demo')
 st.markdown('---')
-st.markdown('## This is a streamlit app')
+st.input('Adukko', max_chars=1000)
 st.markdown('---')
 
 
