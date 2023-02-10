1b0f6f94115a8d9a2eb8c8d78221d6cb4bcab599
Sahas <sahas@tamarind.ai>
2023-02-10 23:49:57 +0530

    Modified input field

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,11 @@ import streamlit as st
 # create a streamlit app with a title and a max width of 1000px
 st.title('Kastam GPT Demo')
 st.markdown('---')
-st.input('Adukko', max_chars=1000)
-st.markdown('---')
+user_input = st.text_input("Enter your message:", max_chars = 1000)
+
+if user_input:
+    response = "Hello! How can I help you today?"
+    st.write("Bot:", response)
 
 
 
