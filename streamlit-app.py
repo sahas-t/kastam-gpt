50585f44e9e0157d064f6b0e2a61626a523aa1db
Sahas <sahas@tamarind.ai>
2023-02-10 23:33:29 +0530

    Basic streamlit app

---
+++ streamlit-app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+# create a streamlit app with a title and a max width of 1000px
+st.title('Streamlit App')
+st.markdown('---')
+st.markdown('## This is a streamlit app')
+st.markdown('---')
+
+
+
+
+
+
+
+
+
+
